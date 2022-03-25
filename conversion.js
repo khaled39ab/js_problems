4eecf9508f5f8ed1c24b90bbb2398433763e2c0c
khaled39ab <khaled39ab@gmail.com>
2022-03-25 15:53:55 +0600

    conversion

---
+++ conversion.js
@@ -0,0 +1,11 @@
+function feetToInch(feet){
+    return feet * 12;
+}
+
+console.log(feetToInch(10) + " Inches");
+
+function centimeterToMeter(centimeter){
+    return  centimeter/100;
+}
+
+console.log(centimeterToMeter(500) + " Meter");
