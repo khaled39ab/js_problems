d14e00a0ea45cafb9c5b8cd7e6dfa5ae352b4d39
khaled39ab <khaled39ab@gmail.com>
2022-03-25 11:34:43 +0600

    revise array

---
+++ destroyAnArray.js
@@ -0,0 +1,28 @@
+const num = [14, 25, 36, 47, 58, 69]; //declare an array
+console.log(num.length);  //number of elements
+
+num[4] = 30; // update 4th position in array
+console.log(num[4]);
+console.log(num);
+
+
+num.push(10);  //add a num
+console.log(num);
+num.pop() //remove a num
+console.log(num);
+
+num.unshift(100); //add number from first
+console.log(num);
+num.shift(); //remove number from first
+console.log(num);
+
+
+function isExist(number){
+    if (num.indexOf (number) !== -1){
+        console.log(number + " is exist in array");
+    } else {
+        console.log(number + " is not exist in array");
+    }
+}
+
+isExist(25);
